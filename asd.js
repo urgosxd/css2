5b25ede906363cfcbdbe1a21e474eb795d42612b
urgosxd <60458146+urgosxd@users.noreply.github.com>
2020-08-30 20:41:06 -0500

    asd

--- asd.js
+++ asd.js
@@ -2,3 +2,4 @@ const A1 = [{ hola: "nada" }, { hola: "todo" }, { hola: "ga" }]
 const A2 = [{ hola: "nadaa" }, { hola: "todos" }, { hola: "ga" }]
 
 A1.forEach(ele => console.log(A2.find(caca => caca.hola == ele.hola)))
+s
